b27c2601ecb5d1c62f2a81f34e4b6bbec5bee843
FelipeDaros <felipe-daros@hotmail.com>
2023-11-07 01:41:05 -0300

    Ajustes

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// index.js
-
 const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
 const app = express();
-const cors = require('cors'); // Importe o middleware cors
-
+const cors = require('cors');
+const path = require('path'); // Importe a biblioteca 'path'
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
+
+app.set('views', path.join(__dirname, 'views')); // Define o diretório de visualizações
+app.set('view engine', 'ejs'); // Define o mecanismo de visualização (neste caso, 'ejs')
+app.use(express.static( "public" ));
+
 
 const port = 3333;
 
@@ -23,7 +26,7 @@ sequelize.authenticate().then(() => {
   console.error('Erro ao conectar ao banco de dados:', error);
 });
 
-const Cadastro = sequelize.define('Cadastro', {
+const Eletrecista = sequelize.define('eletrecista', {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
@@ -33,49 +36,146 @@ const Cadastro = sequelize.define('Cadastro', {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  data_nascimento: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  cidade: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
   },
+  custo_medio: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+  },
+  especialidades: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  }
+});
+
+const Cliente = sequelize.define('cliente', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cidade: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 // Sincronize o modelo com o banco de dados
-Cadastro.sync()
+Eletrecista.sync()
   .then(() => {
-    console.log('Tabela "Cadastros" sincronizada com sucesso.');
+    console.log('Tabela "Eletrecista" sincronizada com sucesso.');
   })
   .catch(error => {
-    console.error('Erro ao sincronizar a tabela "Cadastros":', error);
+    console.error('Erro ao sincronizar a tabela "Eletrecista":', error);
   });
 
-app.post('/cadastrar', async (req, res) => {
+Cliente.sync()
+.then(() => {
+  console.log('Tabela "Cliente" sincronizada com sucesso.');
+})
+.catch(error => {
+  console.error('Erro ao sincronizar a tabela "Cliente":', error);
+});
+
+app.post('/cadastrar-eletrecista', async (req, res) => {
   try {
-    const novoUsuario = await Cadastro.create({
+    const eletrecista = await Eletrecista.create({
       nome: req.body.nome,
       email: req.body.email,
+      data_nascimento: req.body.data_nascimento,
+      cidade: req.body.cidade,
+      custo_medio: req.body.custo_medio,
+      especialidades: req.body.especialidades,
     });
 
-    res.status(201).json(novoUsuario);
+    res.status(201).json(eletrecista);
   } catch (error) {
     console.error('Erro ao inserir usuário:', error);
     res.status(500).json({ error: 'Erro ao inserir usuário' });
   }
 });
 
-app.get('/listar', async (req, res) => {
+app.get('/listar-eletrecista', async (req, res) => {
   try {
-    const usuarios = await Cadastro.findAll();
+    const eletrecistas = await Eletrecista.findAll();
 
-    res.json(usuarios);
+    res.json(eletrecistas);
   } catch (error) {
-    console.error('Erro ao listar usuários:', error);
-    res.status(500).json({ error: 'Erro ao listar usuários' });
+    console.error('Erro ao listar eletrecistas:', error);
+    res.status(500).json({ error: 'Erro ao listar eletrecistas' });
   }
 });
 
+app.post('/cadastrar-cliente', async (req, res) => {
+  try {
+    const cliente = await Cliente.create({
+      nome: req.body.nome,
+      cpf: req.body.cpf,
+      cidade: req.body.cidade,
+      email: req.body.email,
+      senha: req.body.senha,
+    });
+
+    res.status(201).json(cliente);
+  } catch (error) {
+    console.error('Erro ao inserir usuário:', error);
+    res.status(500).json({ error: 'Erro ao inserir usuário' });
+  }
+});
+
+app.get('/listar-clientes', async (req, res) => {
+  try {
+    const clientes = await Cliente.findAll();
+
+    res.json(clientes);
+  } catch (error) {
+    console.error('Erro ao listar clientes:', error);
+    res.status(500).json({ error: 'Erro ao listar clientes' });
+  }
+});
+
+app.get('/home', async (req, res) => {
+  res.render('home'); // Remova a parte './views/'
+});
+
+app.get('/cadastro-eletrecista', async (req, res) => {
+  res.render('cadastro-eletrecista'); // Remova a parte './views/'
+});
+
+app.get('/cadastro-cliente', async (req, res) => {
+  res.render('cadastro-cliente'); // Remova a parte './views/'
+});
+
 app.listen(port, () => {
-  console.log("Servidor rodando na porta 3333");
+  console.log(`Servidor rodando na porta ${port}`);
 });
 
 module.exports = { sequelize };
