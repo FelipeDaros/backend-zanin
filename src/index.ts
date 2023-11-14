386f507a083fdf4f3becf355344de586c7ca7a41
FelipeDaros <felipe-daros@hotmail.com>
2023-11-14 18:25:54 -0300

    Ajustes

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ AppDataSource.initialize().then(() => {
 
   app.use(cors());
 
-  app.set('views', path.join(__dirname ,'views')); // Define o diretório de visualizações
+  app.set('views', path.join(__dirname, 'views')); // Define o diretório de visualizações
   app.set('view engine', 'ejs'); // Define o mecanismo de visualização (neste caso, 'ejs')
   app.use(express.static("public"));
 
@@ -30,25 +30,25 @@ AppDataSource.initialize().then(() => {
       });
 
       await AppDataSource.getRepository(Eletrecista).save(eletrecista);
-  
+
       return res.status(201).json(eletrecista);
     } catch (error) {
       console.error('Erro ao inserir usuário:', error);
       res.status(500).json({ error: 'Erro ao inserir usuário' });
     }
   });
-  
+
   app.get('/listar-eletrecista', async (req: Request, res: Response) => {
     try {
       const eletrecistas = await AppDataSource.getRepository(Eletrecista).find();
-      
+
       return res.json(eletrecistas);
     } catch (error) {
       console.error('Erro ao listar eletrecistas:', error);
       res.status(500).json({ error: 'Erro ao listar eletrecistas' });
     }
   });
-  
+
   app.post('/cadastrar-cliente', async (req, res) => {
     try {
       const cliente = AppDataSource.getRepository(Cliente).create({
@@ -61,18 +61,18 @@ AppDataSource.initialize().then(() => {
       });
 
       await AppDataSource.getRepository(Cliente).save(cliente);
-  
+
       return res.status(201).json(cliente);
     } catch (error) {
       console.error('Erro ao inserir usuário:', error);
       res.status(500).json({ error: 'Erro ao inserir usuário' });
     }
   });
-  
+
   app.get('/listar-clientes', async (req, res) => {
     try {
       const clientes = await AppDataSource.getRepository(Cliente).find();
-  
+
       return res.json(clientes);
     } catch (error) {
       console.error('Erro ao listar clientes:', error);
@@ -82,16 +82,32 @@ AppDataSource.initialize().then(() => {
 
 
   app.get('/home', async (req, res) => {
-    res.render('home'); // Remova a parte './views/'
+    res.render('home');
+  });
+
+  app.get('/c-home', async (req, res) => {
+    res.render('c-home');
+  });
+
+  app.get('/c-contratar', async (req, res) => {
+    res.render('c-contratar');
+  });
+
+  app.get('/c-agenda', async (req, res) => {
+    res.render('c-agenda');
+  });
+
+  app.get('/c-contratar-agenda-pagamento', async (req, res) => {
+    res.render('c-contratar-agenda-pagamento');
   });
 
   app.get('/cadastro-eletrecista', async (req, res) => {
-    res.render('cadastro-eletrecista'); // Remova a parte './views/'
+    res.render('cadastro-eletrecista');
   });
 
   app.get('/cadastro-cliente', async (req, res) => {
-    res.render('cadastro-cliente'); // Remova a parte './views/'
+    res.render('cadastro-cliente');
   });
- 
+
   return app.listen(process.env.PORT)
 })
